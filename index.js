84ffe85661670ee106a582f1b0c9cc21fc1ac681
C-MAN <steveseaman@MacBook-Pro.okcu.edu>
2023-10-19 15:24:20 -0500

    starting to look like a website

--- index.js
+++
@@ -1,9 +0,0 @@
-var c = document.getElementById("canvas");
-var context = c.getContext("2d"); 
-
-function alertButton(){
-	alert("Hello, world!");
-}
-
-document.getElementById("button").addEventListener("click",alertButton); 
-
